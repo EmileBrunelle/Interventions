15c420f8e29792e26a80d5ceda47aa281fbb8081
Émile Brunelle <ebrunelle08@gmail.com>
2018-04-25 15:40:27 -0400

    Commit de la fin du cours

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProblemeComponent } from './probleme.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { TypeProblemeService } from './typeprobleme.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ProblemeComponent', () => {
   let component: ProblemeComponent;
@@ -9,8 +11,9 @@ describe('ProblemeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ReactiveFormsModule],
-      declarations: [ ProblemeComponent ]
+      imports: [ReactiveFormsModule, HttpClientModule],
+      declarations: [ ProblemeComponent ],
+      providers:[TypeProblemeService]
     })
     .compileComponents();
   }));
@@ -35,5 +38,19 @@ describe('ProblemeComponent', () => {
     let zone = component.problemeForm.controls['prenomUtilisateur'];
     zone.setValue('a'.repeat(3));
     expect(zone.valid).toBeTruthy();
+  });
+
+  it('Zone SMS est désactivée si Ne pas notifier ou courriel', () => {
+    component.gestionNotifications('ParCourriel');
+
+    let zone = component.problemeForm.get('notificationGroup.modeNotification');
+    expect(zone.status).toEqual('DISABLED');
+  });
+
+  it('Zone Courriel est désactivée si Ne pas notifier ou SMS', () => {
+    component.gestionNotifications('ParSMS');
+
+    let zone = component.problemeForm.get('notificationGroup.modeNotification');
+    expect(zone.status).toEqual('DISABLED');
   });
 });
--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -20,11 +20,38 @@ export class ProblemeComponent implements OnInit {
     this.problemeForm = this.fb.group({
       prenomUtilisateur: ['', [Validators.required, VerifierCaracteresValidator.longueurValide(3)]],
       nomUtilisateur: ['', [Validators.required, VerifierCaracteresValidator.longueurValide(3)]],
-      noProbleme: ['']
+      noProbleme: [''],
+      notifier:['Aucun'],
+      notificationGroup: this.fb.group({
+        modeNotification: [{value: ''}]
+      })
     });
 
     this.problemes.obtenirTypesProbleme()
     .subscribe(probleme => this.typesProblemes = probleme,
               error => this.errorMessage = <any>error);              
+  }
+
+  gestionNotifications(typeNotification: string): void {
+    const courrielControl = this.problemeForm.get('notificationGroup.mode');
+    const smsControl = this.problemeForm.get('notificationGroup.mode');
+
+    courrielControl.clearValidators();
+    courrielControl.reset();
+    courrielControl.disable();
+
+    smsControl.clearValidators();
+    smsControl.reset();
+    smsControl.disable();
+
+    if (typeNotification === 'ParCourriel') {
+      courrielControl.enable();
+      courrielControl.setValidators([Validators.required]);
+    } else if (typeNotification === 'ParSMS') {
+      smsControl.enable();
+      smsControl.setValidators([Validators.required]);
+    }
+    courrielControl.updateValueAndValidity();
+    smsControl.updateValueAndValidity();
   }
 }
